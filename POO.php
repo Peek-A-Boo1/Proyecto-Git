e2a58059b832ec78ee82ec66728ca11910e535cb
Ramon <megaantilimite@gmail.com>
2021-02-17 15:18:12 -0600

    Nuevo contenido de POO.php

--- POO.php
+++ POO.php
@@ -1,13 +1,21 @@
 <?php
-/*PROGRAMACION ORIENTADA A OBJETOS EN PHP*/
+echo "<h3>PROGRAMACION ORIENTADA A OBJETOS EN PHP</h3><br>";
 
 echo "La programacion orientada a objetos es un paradigma de programacion;es decir que nos referimos a una tecnica de programacion que ha sido <br>
       estudiada y discutida por una comunidad de expertos o interesados en el tema con el unico fin  programar mas y codificar menos.<br>
      La programacion orienta a objetos es el paradigma usado por la comunidad de programadores ademas de ser un paradigma universal <br>
      en los diferentes lenguajes de progrmacion exitentes y por existir. <BR> <BR>";
-echo "POO  se divide en 4 conceptos importantes :Metodos,Instancias,Clases y propiedades";
+echo "POO  se divide en 4 conceptos importantes :Metodos,Instancias,Clases y propiedades<br>";
 
 echo " Las clases son el molde donde se ingresan los metodos y propiedades.El objetivo de la clase es crear un plano lleno de instruciones que dan 
-forma a ala misma";
+forma a ala misma<br>";
+
+echo "Las propiedades son variables que son declaradas dentro de la clase.Una variable en el lenguaje cotitiano puede asemejarze a un adjetivo
+      ya que estas describen a la clase.";
+
+echo "La palabra metodo es la forma en la que se conocen a las funciones dentro de la programacion orientada a objetos.Los metodos<br>
+ funcionan como los verbos del lenguaje convencional, su principal mision dentro de la Programacion es realizar una determianada accion.";
+
+
 
 ?>
